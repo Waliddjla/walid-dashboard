05789d423f3621d5b8cefffe629d967007326518
djellali <walidbibo1997@gmail.com>
2023-06-09 21:03:50 +0100

    first commit

---
+++ src/Pages/DashboardEmployees/DashboardEmp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DashboardEmp() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
---
+++ src/Pages/DashboardFinance/DashboardFin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DashboardFin() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
