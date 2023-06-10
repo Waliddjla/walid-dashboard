16f11791bf42ccd0843a4c37ae5e9711261bf2f1
djellali <walidbibo1997@gmail.com>
2023-06-10 18:28:51 +0100

    first barchart

--- src/Pages/DashboardFinance/DashboardFin.js
+++ src/Pages/DashboardFinance/DashboardFin.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
 import './DashboardFinance.css';
+import Barchart from '../../Components/Sidebar/AllChart/Barchart';
 
 export default function DashboardFin() {
   return (
-    <div>
+    <div className='global-container'>
+
+    <h1>Les résiltats de l'année: 2023</h1>
+    <form>
+      <label htmlFor="year">Choisissez une date</label>
+      <select id="year">
+        <option value="2023">2023</option>
+        <option value="2022">2022</option>
+        <option value="2021">2021</option>
+        <option value="2021">2020</option>
+      </select>
+    </form>
+    <div className="dashboard-container">
+    <Barchart />
+      
+    </div>
+
       
     </div>
   )
